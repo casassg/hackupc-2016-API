9bfcb71138e5e904a46e09b752089fbddc7e4fee
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-09-14 19:53:27 +0200

    add adult field to extractor

--- algorithm/extractor.py
+++ algorithm/extractor.py
@@ -17,11 +17,19 @@ def need_travel(obj):
 	else:
 		return 'free'
 
+def is_adult(obj):
+	data = json.loads(obj['data'])
+	if 'adult' in data and data['adult'] == '1':
+		return 'adult'
+	else:
+		return 'minor'
+
+
 conn = sqlite3.connect('../backend/api/tmp/test.db')
 conn.row_factory = sqlite3.Row
 rows = conn.execute('SELECT * FROM applications WHERE state=?', (state,))
 
-applications = [(appl["id"], appl["name"].encode('utf-8'), appl["email"].encode('utf-8'), need_travel(appl)) for appl in rows]
+applications = [(appl["id"], appl["name"].encode('utf-8'), appl["email"].encode('utf-8'), need_travel(appl), is_adult(appl)) for appl in rows]
 
 with open(state+'.csv', 'wb') as f:
     writer = csv.writer(f)
